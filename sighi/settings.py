5769718800b33b46d84ebea32a61d072bddc9d5d
jhan aguirre <jhan.aguirreg@gmail.com>
2026-08-24 11:11:41 -0500

    Configurar host  para Render

--- sighi/settings.py
+++ sighi/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
 SECRET_KEY = 'jhanprueba'
 
-DEBUG = True
+DEBUG = false
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
